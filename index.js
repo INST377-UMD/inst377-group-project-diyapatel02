4b8864781c1bfbf81c8be9f3273e6557242cf4e2
supperdaggie <89858316+supperdaggie@users.noreply.github.com>
2023-12-19 12:28:18 -0500

    updated final

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const supabaseClient = require('@supabase/supabase-js')
 const app = express()
-const port = 3000;
+const port = 4000;
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(__dirname + '/public'));
 
